6aa5db5a667c7800b38be5ab3c5cf4d66479e2aa
Mo <Radiergummi@users.noreply.github.com>
2018-06-21 14:37:28 +0200

    Update MakePlugin.js

--- src/Core/Plugins/MakePlugin.js
+++ src/Core/Plugins/MakePlugin.js
@@ -1,12 +1,12 @@
 'use strict';
 
-const CorePlugin = require('../Plugin');
+const CorePlugin       = require('../Plugin');
 
 const ComponentCommand = require('./MakeCommands/ComponentCommand');
-const MixinCommand = require('./MakeCommands/MixinCommand');
-const RouteCommand = require('./MakeCommands/RouteCommand');
-const ModuleCommand = require('./MakeCommands/ModuleCommand');
-const StyleCommand = require('./MakeCommands/StyleCommand');
+const MixinCommand     = require('./MakeCommands/MixinCommand');
+const RouteCommand     = require('./MakeCommands/RouteCommand');
+const ModuleCommand    = require('./MakeCommands/ModuleCommand');
+const StyleCommand     = require('./MakeCommands/StyleCommand');
 
 class MakePlugin extends CorePlugin {
   static get name() {

c3f6bc7b43ba0f35e6ae9f6a608d5ff7c698ebbd
Mo <Radiergummi@users.noreply.github.com>
2018-06-21 14:10:17 +0200

    Create CommandExecutionError.js

---
+++ src/Errors/CommandExecutionError.js
@@ -0,0 +1,19 @@
+'use strict';
+
+class CommandExecutionError extends Error {
+  constructor(plugin, handler, error) {
+    super(error.message);
+
+    this.name = this.constructor.name;
+    
+    this.meta = { plugin, handler, error };
+
+    if (typeof Error.captureStackTrace === 'function') {
+      Error.captureStackTrace(this, this.constructor);
+    } else { 
+      this.stack = (new Error(message)).stack; 
+    }
+  }
+}
+
+module.exports = CommandExecutionError;
